17b6f14f2feab78b74e0fb7c42c6377cd61fd0e9
RJB201708 <RJB201708@gmail.com>
2017-09-03 00:24:34 -0700

    User Function added

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-
 var restify = require('restify');
 var plugins = require('restify-plugins');
 var restifyValidator = require('restify-validator');
+
 var mongoose = require('mongoose');
-var server = restify.createServer();
-var setupController = require('./controllers/setupController.js');
-var userController = require('./controllers/userController.js');
-var config = require('./config/dbConnection.js');
+
+var setupController = require('./server/controllers/setupController.js');
+var userController = require('./server/controllers/userController.js');
+var config = require('./server/config/dbConnection.js');
 
 mongoose.connect(config.getMongoConnection());
+
+var server = restify.createServer();
 setupController(server, restify, restifyValidator);
 userController(server);
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(3000, function() {
+    console.log('%s listening at %s', server.name, server.url);
 });
-
